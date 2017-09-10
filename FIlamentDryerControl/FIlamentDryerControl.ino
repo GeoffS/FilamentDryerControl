f0790c3516a7169302afc67b6a55111859071cf9
Geoff Sobering <geoff@geoff-s.net>
2017-09-09 22:04:36 -0500

    First commit. Simple test code for the solid-state relay.

---
+++ FIlamentDryerControl/FIlamentDryerControl.ino
@@ -0,0 +1,19 @@
+const int RELAY_PIN = 6;
+const int LED_PIN = 13;
+const int delay_ms = 2000;
+
+void setup() 
+{
+  pinMode(RELAY_PIN, OUTPUT);
+  pinMode(LED_PIN, OUTPUT);
+}
+
+void loop() 
+{
+    digitalWrite(RELAY_PIN, LOW);
+    digitalWrite(LED_PIN, LOW);
+    delay(delay_ms);
+    digitalWrite(RELAY_PIN, HIGH);
+    digitalWrite(LED_PIN, HIGH);
+    delay(delay_ms);
+}
--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # FilamentDryerControl
+
+This project is an Arduino controller for a simple convetion oven
+to improve the temerature control between 60C and 80C.

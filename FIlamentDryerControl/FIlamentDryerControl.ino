02feb2947220a64608d3dfd3dcbab5c5bab8f110
Geoff Sobering <geoff@geoff-s.net>
2017-09-23 18:14:39 -0500

    First version running with oven.

--- FIlamentDryerControl/FIlamentDryerControl.ino
+++ FIlamentDryerControl/FIlamentDryerControl.ino
@@ -1,4 +1,4 @@
-const int RELAY_PIN = 6;
+const int RELAY_PIN = 5;
 const int LED_PIN = 13;
 const int delay_ms = 5000;
 
@@ -8,21 +8,23 @@ int ktcSO = 8;
 int ktcCS = 9;
 int ktcCLK = 10;
 
-float setPoint_C = 35;
+float setPoint_C = 65;
 float hysterisis_C = 1;
 
 float upperSetPoint_C = setPoint_C + hysterisis_C/2;
 float mediumLowSetPoint_C = setPoint_C - hysterisis_C/2;
 float lowerSetPoint_C = setPoint_C - hysterisis_C;
 
-float equlibDutyCycle = 0.3;
-float mediumLowEqulibDutyCycle = 0.155;
+float lowDutyCycle = 0.3;
+float equlibDutyCycle = 0.1;
+float mediumLowEqulibDutyCycle = 0.05;
+
+bool startupCtr;
+float startupTemp_C = 45;
 
 unsigned long equilibOnTime_ms = delay_ms * equlibDutyCycle;
-//unsigned long equilibOffTime_ms = delay_ms - equilibOnTime_ms;
-
+unsigned long lowOnTime_ms = delay_ms * lowDutyCycle;
 unsigned long mediumLowEquilibOnTime_ms = delay_ms * mediumLowEqulibDutyCycle;
-//unsigned long mediumLowEquilibOffTime_ms = delay_ms - mediumLowEquilibOnTime_ms;
 
 MAX6675 ktc(ktcCLK, ktcCS, ktcSO);
 
@@ -34,14 +36,34 @@ void setup()
 
   digitalWrite(RELAY_PIN, LOW);
   digitalWrite(LED_PIN, LOW);
+
+   printVar("equilibOnTime_ms", equilibOnTime_ms);
+   printVar("mediumLowEquilibOnTime_ms", mediumLowEquilibOnTime_ms);
+   printVar("lowOnTime_ms", lowOnTime_ms);
   
   delay(500);
+
+  startupCtr = 0;
 }
 
 void loop() 
 {
   float deg_C = ktc.readCelsius();
   Serial.print(deg_C);
+
+  if(startupCtr > 10)
+  {
+    heaterOn();
+    startupCtr++;
+    if(deg_C > startupTemp_C)
+    {
+      heaterOff();
+      startupCtr = 100;
+      Serial.println("Too Hot");
+      return;
+    }
+    delay(2000);
+  }
 
   if((deg_C <= upperSetPoint_C) && (deg_C >= lowerSetPoint_C))
   {
@@ -54,11 +76,13 @@ void loop()
     }
     Serial.print(",");
     Serial.println(onTime_ms);
-    digitalWrite(RELAY_PIN, HIGH);
-    digitalWrite(LED_PIN, HIGH);
+    heaterOn();
+//    digitalWrite(RELAY_PIN, HIGH);
+//    digitalWrite(LED_PIN, HIGH);
     delay(onTime_ms);
-    digitalWrite(RELAY_PIN, LOW);
-    digitalWrite(LED_PIN, LOW);
+    heaterOff();
+//    digitalWrite(RELAY_PIN, LOW);
+//    digitalWrite(LED_PIN, LOW);
     delay(delay_ms-onTime_ms);
     return;
   }
@@ -67,9 +91,17 @@ void loop()
   if(deg_C < upperSetPoint_C)
   {
     // Too cold:
-    Serial.println(",20");
-    digitalWrite(RELAY_PIN, HIGH);
-    digitalWrite(LED_PIN, HIGH);
+    //Serial.println(",20");
+    Serial.print(",");
+    float onTime_ms = lowOnTime_ms;
+    Serial.println(onTime_ms);
+    heaterOn();
+    delay(onTime_ms);
+    heaterOff();
+    delay(delay_ms-onTime_ms);
+    return;
+//    digitalWrite(RELAY_PIN, HIGH);
+//    digitalWrite(LED_PIN, HIGH);
   }
   
   if(deg_C > setPoint_C-hysterisis_C)
@@ -82,3 +114,22 @@ void loop()
 
   delay(delay_ms);
 }
+
+void heaterOn()
+{
+  digitalWrite(RELAY_PIN, HIGH);
+  digitalWrite(LED_PIN, HIGH);
+}
+
+void heaterOff()
+{
+  digitalWrite(RELAY_PIN, LOW);
+  digitalWrite(LED_PIN, LOW);
+}
+
+void printVar(String name, unsigned long value)
+{
+  Serial.print(name+" ");
+  Serial.println(value);
+}
+

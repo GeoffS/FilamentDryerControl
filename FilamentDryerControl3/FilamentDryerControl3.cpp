fe6ae8b485d887e9ac33c43216664b919dac785c
Geoff Sobering <geoff@geofffs.net>
2018-08-23 14:22:54 -0500

    Added the required libraries.

--- FilamentDryerControl3/FilamentDryerControl3.cpp
+++ FilamentDryerControl3/FilamentDryerControl3.cpp
@@ -1,9 +1,23 @@
 #include <Arduino.h>
 
-void setup() {
+// Include the tasks framework:
+#include <GSSArduTasks.h>
+
+// Include the GSSUtil Button and Blinker classes:
+#include <Button.h>
+#include <Blinker.h>
+// Include the thermocouple library:
+#include "max6675.h"
+
+// include the LCD library code:
+#include <LiquidCrystal.h>
+
+void setup()
+{
 
 }
 
-void loop() {
+void loop()
+{
 
 }
